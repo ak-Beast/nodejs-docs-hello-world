ced895580e56edfda66ab13639f88bb9d3f0b995
ak-Beast <94402509+ak-Beast@users.noreply.github.com>
2024-10-16 13:34:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 // ***************************************************************************
 
 response.end("Hello World!");
-response.end("Hello World1!");
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
